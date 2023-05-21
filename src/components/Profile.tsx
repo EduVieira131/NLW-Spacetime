8d54aa4fd9efb9d8073901cf30cbc5eacbfb3057
EduViera131 <eduviera369@gmail.com>
2023-05-20 22:09:07 -0300

    Create memory page created

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,8 +4,6 @@ import Image from 'next/image'
 export function Profile() {
   const { name, avatarURL } = getUser()
 
-  console.log(avatarURL)
-
   return (
     <div className="flex items-center gap-3 text-left">
       <Image
@@ -18,7 +16,10 @@ export function Profile() {
 
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <a href="#" className="block text-red-400 hover:text-red-300">
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
           Quero sair
         </a>
       </p>
